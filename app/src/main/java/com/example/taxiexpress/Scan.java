a4fd5334176f77c07ad81268af2b533399039c91
MunDo <stewie-z@hotmail.com>
2020-07-13 00:46:15 -0400

    Navigation Bar tested and working

--- app/src/main/java/com/example/taxiexpress/Scan.java
+++ app/src/main/java/com/example/taxiexpress/Scan.java
@@ -9,35 +9,35 @@ import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class HomeScreen extends AppCompatActivity {
+public class Scan extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_screen);
+        setContentView(R.layout.activity_scan);
         //Initialize and assign variables
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         //Set Taxi selected
-        bottomNavigationView.setSelectedItemId(R.id.tracktaxi);
+        bottomNavigationView.setSelectedItemId(R.id.scan);
         //Perform Item Selected Listener
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch(menuItem.getItemId()){
                     case R.id.tracktaxi:
+                        startActivity(new Intent(getApplicationContext(),
+                                HomeScreen.class));
+                        overridePendingTransition(0,0);
                         return true;
                     case R.id.routes:
                         startActivity(new Intent(getApplicationContext(),Routes.class));
                         overridePendingTransition(0,0);
                         return true;
                     case R.id.profile:
-                        startActivity(new Intent(getApplicationContext(),
-                                Profile.class));
+                        startActivity(new Intent(getApplicationContext(),Profile.class));
                         overridePendingTransition(0,0);
                         return true;
                     case R.id.scan:
-                        startActivity(new Intent(getApplicationContext(),Scan.class));
-                        overridePendingTransition(0,0);
                         return true;
                 }
                 return false;
