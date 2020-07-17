0e0d631ba96bfdc87d5156b7f3848413a741b6ae
Richard Pitter <pitterrichard0072@gmail.com>
2020-07-17 16:34:26 -0500

    Qr scanner is working with a bit of UI problems.

--- app/src/main/java/com/example/taxiexpress/Scan.java
+++ app/src/main/java/com/example/taxiexpress/Scan.java
@@ -3,18 +3,63 @@ package com.example.taxiexpress;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class Scan extends AppCompatActivity {
-
+    CodeScanner codeScanner;
+    CodeScannerView scannView;
+    TextView resultData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+        scannView = findViewById(R.id.scannerView);
+        codeScanner = new CodeScanner(this,scannView );
+        resultData = findViewById(R.id.Resultqr);
+
+
+        codeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        resultData.setText(result.getText());
+
+                    }
+                });
+            }
+        });
+
+        scannView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                codeScanner.startPreview();
+            }
+        });
+
+
+
+
         //Initialize and assign variables
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         //Set Taxi selected
@@ -43,5 +88,28 @@ public class Scan extends AppCompatActivity {
                 return false;
             }
         });
+    }
+    protected void onResume() {
+        super.onResume();
+        requestForCamera();
+    }
+
+    private void requestForCamera() {
+        Dexter.withActivity(this).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                codeScanner.startPreview();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                Toast.makeText(Scan.this, "Camera Permission is Required", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+            }
+        }).check();
     }
 }
