4679ba398d9c7c1c0410116eea6c3a7b222fd9e6
MunDo <stewie-z@hotmail.com>
2020-07-17 22:37:06 -0400

    Qr scanner code added

--- app/src/main/java/com/example/taxiexpress/MainActivity.java
+++ app/src/main/java/com/example/taxiexpress/MainActivity.java
@@ -47,7 +47,7 @@ private static int SPLASH_TIME_OUT=4000;
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent homeIntent = new Intent (MainActivity.this, Login.class);
+                Intent homeIntent = new Intent (MainActivity.this, Profile.class);
                 startActivity(homeIntent);
                 finish();
             }
